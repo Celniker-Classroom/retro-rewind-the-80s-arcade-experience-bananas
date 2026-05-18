c2e60751f7713b16e24b7638293f99da28ece663
CaydenArlett <caydenarlett15@gmail.com>
2026-05-18 20:46:55 +0000

    All the work from last week is in this push because I was having trouble pushing. Added basic movement for the character, added platforms that scroldonwards, added scaling to the scroling speed, added a spawner that spanws barels that end the game if they hit you but you can jump off of the top of, added a object that clears sprites that go below the screen and added something to spawn new tiles at the top as the game progresses

--- sketch.js
+++ sketch.js
@@ -1,105 +1,241 @@
-await Canvas(360, 640);
-world.gravity.y = 0.8;
-world.frameRate = 60;
+await Canvas();
+world.gravity.y = 7;
 
-let player = new Sprite();
-player.diameter = 44;
-player.color = 'cyan';
-player.physics = DYNAMIC;
-player.x = 0;
-player.y = 180;
-player.restitution = 0;
-player.friction = 0.2;
-player.maxSpeed.y = 18;
+//static variables
+const barelSpeed = 4;
+const barelJumpH = 40;
+const barelJumpW = 90/2;
+const jump = -8;
+const acel = 0.5;
+const speed = 5;
+const jumpFrames = 15;
+const scaleSpeed = 0.05;
 
-let leftWall = new Sprite();
-leftWall.physics = STATIC;
-leftWall.width = 24;
-leftWall.height = 1800;
-leftWall.x = -160;
-leftWall.y = 0;
-leftWall.color = '#333';
+//changing  variables
+let scroolSpeed = 0.8;
+let score = 0;
+let frameTime = 0;
+let frames = 0;
+let onGround = false;
 
-let rightWall = new Sprite();
-rightWall.physics = STATIC;
-rightWall.width = 24;
-rightWall.height = 1800;
-rightWall.x = 160;
-rightWall.y = 0;
-rightWall.color = '#333';
+//decleration of player sprite
+let player = new Sprite(0,0, 50, DYNAMIC);
+player.img = '🤪';
 
-let floor = new Sprite();
-floor.physics = STATIC;
-floor.width = 320;
-floor.height = 18;
-floor.x = 0;
-floor.y = 300;
-floor.color = '#228B22';
+//decleration of barrel spanwers
+let spawner = new Group();
+spawner.physics = STATIC;
+spawner.w = 40;
+spawner.h = 40;
+spawner.vel.y = scroolSpeed;
 
-let platforms = [];
-for (let i = 0; i < 10; i++) {
-	let platform = new Sprite();
-	platform.physics = STATIC;
-	platform.width = 120;
-	platform.height = 18;
-	platform.x = random(-100, 100);
-	platform.y = 220 - i * 120;
-	platform.color = '#999';
-	platforms.push(platform);
+let spawn1 = new spawner.Sprite(40,-300);
+
+//decleration of barrel group
+let barels = new Group();
+barels.physics = DYNAMIC;
+barels.d = 30;
+
+//decleration of possible tile spawns for game
+let tiles = [
+	[
+		'p    p    p'
+	]
+];
+
+//starting tiles on the screen
+let startTile = [
+	'p  p  p  p  p',
+	'  p   p    p ',
+	'p   p   p   p',
+	'p  p  p  p  p',
+	'  p   p    p ',
+	'p   p   p   p'
+];
+
+// physical bariar on edge of screen
+let walls = new Group();
+walls.physics = STATIC;
+walls.width = 10; 
+walls.height = 100;
+for (let i = 0; i < 10; i++){
+	let y = (i-5)*100;
+	let a = new walls.Sprite(-755, y);
+	let b = new walls.Sprite(755, y);
 }
 
-let cameraY = 0;
-let gameState = 'play';
-let score = 0;
 
-function playerOnGround() {
-	if (player.collides(floor)) return true;
-	for (let platform of platforms) {
-		if (player.collides(platform)) return true;
+//platforms that player jumps on
+let platforms = new Group();
+platforms.width = 100;
+platforms.height = 10;
+platforms.tile = "p";
+platforms.physics = KIN;
+platforms.vel.y = scroolSpeed;
+platforms.addTiles(startTile, -700, -665, 100, 200);
+function createPlatform(x, y){
+ 	let plat = new platforms.Sprite(x, y);
+ }
+createPlatform(0, 300);
+
+//object below the screen that destroys sprites that hit it
+let floor = new Sprite(0,450,1600,50,STATIC);
+
+//returns true when the player is allowed to jump
+function playerOnGround(){
+	for (let plat of platforms){
+		if (player.collides(plat)){
+			frames = 0;
+			return true;
+		}
+	}
+	for (let plat of barels){
+		let x = plat.x;
+		let y = plat.y;
+		let yOff = player.y-plat.y + player.h/2 + barels.d/2;
+		if (yOff <= 0 && yOff >= -barelJumpH){
+			let xOff = Math.abs(player.x - plat.x - player.w/2 - barels.d/2);
+			if (xOff <= barelJumpW){
+				frames = 8;
+				return true;
+			}
+		}
+	}
+	// if (player.collides(floor)){
+	// 	frames = 0;
+	// 	return true;
+	// }
+	frames ++;
+	if (frames < jumpFrames){
+		return true;
+	}
+	for (let plat of platforms){
+		if (player.colliding(plat)){
+			return true;
+		}
+	}
+	// if (player.colliding(floor)){
+	// 	return true;
+	// }
+	return false;
+}
+
+//moves the player based on input
+function move(){
+	if (keyIsDown(LEFT_ARROW)){
+		if (player.vel.x > -speed){
+			player.vel.x -= acel;
+		}
+	} else if (keyIsDown(RIGHT_ARROW)){
+		if (player.vel.x < speed){
+			player.vel.x += acel;
+		} 
+	}
+	if ((mouse.presses() || kb.presses(' ') || kb.presses('up')) && onGround) {
+		frames = 10;
+		onGround = false;
+		player.vel.y += jump;
+		if (player.vel.y < jump) {
+			player.vel.y = jump;
+		}
+		else if (player.vel.y > jump/2){
+			player.vel.y = jump/2;
+		}
+	}
+}
+
+//spawns barels at spawners once a second
+function spawn(t) {
+	if (t == 60){
+		score++;
+		let offSet = barels.d/2;
+		for (let tempSpawn of spawner){
+			let x = tempSpawn.x;
+			let y = tempSpawn.y - barels.d/2 + spawner.h/2; 
+			let a = new barels.Sprite(x - offSet, y);
+			a.vel.x = - barelSpeed;
+			let b = new barels.Sprite(x + offSet, y);
+			b.vel.x = barelSpeed;
+		}
+	}
+}
+
+// checks if the player hit a barrel
+function checkDefeat(){
+	for (let plat of barels){
+		if (player.collides(plat)){
+			return true;
+		}
 	}
 	return false;
 }
 
+//ends the game and shows title screen
+function endGame(){
+	alert("a");
+}
+floor.overlaps(player, endGame);
+
+//removes objects that hit the floor sprite
+function clearWorld(floor, sprite, dur){
+	sprite.delete();
+}
+
+floor.overlaps(barels, clearWorld);
+floor.overlaps(platforms, clearWorld);
+
+//checks if there are no longer sprites above the vissible screen
+function shouldSpawnTile(){
+	for (let plat of platforms){
+		if (plat.y < -470){
+			return false;
+		}
+	}
+	return true;
+}
+
+//spawns a random tile
+function spawnTile(){
+	platforms.addTiles(tiles[0], -700, -665, 100, 200);
+}
+
+//ajusts position of dynamic objects to acount for scroll speed
+function scroll(){
+	player.y += scroolSpeed/60;
+	for (let plat of barels){
+		plat.y += scroolSpeed/60;
+	}
+}
+
+//scales the speed at which objects scrol donwards on the screen;
+function scaleDifficulty(){
+	if (scroolSpeed < 3){
+		scroolSpeed += scaleSpeed;
+	}
+}
+
+//calls all functions that need to happen when playing the game
+function play() {
+	player.vel.y += 3/60;
+	frameTime++;
+	onGround = playerOnGround();
+	move();
+	spawn(frameTime);
+	scroll();
+	if (checkDefeat()){
+		endGame();
+	}
+	if (frameTime > 59){
+		score++;
+		frameTime = 0;
+		scaleDifficulty();
+		if (shouldSpawnTile()){
+			spawnTile();
+		}
+	}
+}
 q5.update = function () {
-	background('#04111d');
-
-	if (gameState === 'play') {
-		if ((mouse.presses() || keyIsDown(32) || keyIsDown(UP_ARROW)) && playerOnGround()) {
-			player.vel.y = -13;
-		}
-
-		cameraY = min(cameraY, player.y - 160);
-
-		let bottomLimit = cameraY + height / 2 - 24;
-		if (player.y > bottomLimit) {
-			gameState = 'gameover';
-		}
-
-		score = max(score, round(-cameraY));
-	}
-
-	translate(0, -cameraY);
-
-	// Draw world bounds and sprites in world coordinates.
-	fill('#222');
-	rect(-180, cameraY + height / 2 + 120, 360, 240); // lower hazard area below the visible space
-
-	push();
-	translate(0, cameraY);
-	fill('white');
-	textSize(18);
-	textAlign(CENTER, CENTER);
-	text('Vertical Scroller: Jump Up!', 0, -300);
-	textSize(14);
-	text('Stay above the bottom and use the walls to bounce.', 0, -280);
-	text('Score: ' + score, 0, -260);
-
-	if (gameState === 'gameover') {
-		fill('red');
-		textSize(28);
-		text('GAME OVER', 0, -20);
-		textSize(16);
-		text('Refresh to try again.', 0, 20);
-	}
-	pop();
-};
+	background('skyblue');
+	text('click to jump!', 0, -50);
+	play();
+}
